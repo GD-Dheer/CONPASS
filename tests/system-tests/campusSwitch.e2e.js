907770b70a2b710474ab816a8d9806e56e8f507a
Trevor N <trevor.nagg@gmail.com>
2020-04-09 17:42:12 -0400

    [#137] Add a system test for the SGW button

--- tests/system-tests/campusSwitch.e2e.js
+++ tests/system-tests/campusSwitch.e2e.js
@@ -35,3 +35,19 @@ test('campusSwitch - Should click on the Loyola', async () => {
     { action: 'release', options: {} },
     { action: 'tap', options: { x: 806, y: 258 } }]).toStrictEqual(action.toJSON());
 });
+
+test('campusSwitch - Should click on the SGW', async () => {
+  const searchBarXPath = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.EditText';
+  const searchBarElement = await driver.elementsByXPath(searchBarXPath);
+  console.log('AppiumLog-editable searchBar component is present');
+  await driver.clickElement(searchBarElement);
+  const action = new wd.TouchAction(driver);
+  await action.press({ x: 270, y: 270 });
+  action.release();
+  await action.tap({ x: 270, y: 270 });
+  console.log('Tap worked');
+  await driver.performTouchAction(action);
+  expect([{ action: 'press', options: { x: 270, y: 270 } },
+    { action: 'release', options: {} },
+    { action: 'tap', options: { x: 270, y: 270 } }]).toStrictEqual(action.toJSON());
+});
