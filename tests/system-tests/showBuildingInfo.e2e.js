67ceb2464fd2c5992a012af543a8dc3ae53c5b1c
Trevor N <trevor.nagg@gmail.com>
2020-04-11 17:29:27 -0400

    [#137] Add test for indoor building information

--- tests/system-tests/showBuildingInfo.e2e.js
+++ tests/system-tests/showBuildingInfo.e2e.js
@@ -20,18 +20,17 @@ beforeAll(async () => {
   await driver.sleep(58000); // wait for app to load
 });
 
+// Checking the S building specifically
 test('showBuildingInfo', async () => {
   const actionZoomIn = new wd.TouchAction(driver);
-  actionZoomIn.tap({ x: 656, y: 727 });
+  actionZoomIn.tap({ x: 625, y: 742 });
   actionZoomIn.release();
   await driver.performTouchAction(actionZoomIn);
-  console.log('SHOULD BE ZOOMED IN');
   const action = new wd.TouchAction(driver);
-  action.release();
-  action.press({ x: 503, y: 1616 });
-  action.moveTo({ x: 503, y: 379 });
-  action.release();
-  action.tap({ x: 853, y: 1509 });
+  action.tap({ x: 859, y: 1172 });
   action.release();
   await driver.performTouchAction(action);
+  await driver.sleep(5000); // Must wait for the click to fully load
+  el = await driver.elementsByXPath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.HorizontalScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ImageView');
+  expect(el).toBeDefined();
 });
