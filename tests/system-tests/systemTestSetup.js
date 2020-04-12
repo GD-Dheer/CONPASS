5f56ffdb113c0dd61bdd68cf11e7f0479a75e151
Trevor N <trevor.nagg@gmail.com>
2020-04-12 18:05:40 -0400

    [#137] Modify setup to the latest version of the apk and script to the latest test file

--- tests/system-tests/systemTestSetup.js
+++ tests/system-tests/systemTestSetup.js
@@ -1,3 +1,3 @@
-const APK_PATH = 'C:\\Users\\Trevor\\Downloads\\--60c9e2c78ed24f2f961c4e1bdde4fd8b-signed.apk';
+const APK_PATH = 'C:\\Users\\Trevor\\Downloads\\20200411.apk';
 
 export default APK_PATH;
